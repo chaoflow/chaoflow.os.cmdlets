75256f36300cc9a625852093d38594ccea0094dd
Florian Friesdorf <flo@chaoflow.net>
2009-11-05 04:04:35 +0100

    Cmdlet: except for __call__ 100% unittest covered

--- src/chaoflow/os/cmdlets/cmd.py
+++ src/chaoflow/os/cmdlets/cmd.py
@@ -71,87 +71,165 @@ def _exec(*cmdline,**kws):
 
 
 class Cmdlet(object):
-    r"""
+    """
         A command line consists of cmdlets.
 
         Generally a cmdlet is translated into a string with the name of the
-        cmdlet.
+        cmdlet. A series of cmdlets forms a cmdline. Calling a cmdlet, execute
+        the cmdline formed by the series up to this cmdlet (see below).
 
-    A cmdlet has a readonly name which is set during __init__:
-
-        >>> cmdlet1 = Cmdlet('cmd')
-        >>> cmdlet1._name
-        'cmd'
-
-    XXX: not good
-
-        >>> cmdlet1._cmdstr
-        'cmd'
-        >>> cmdlet1._cmdline
-        ['cmd']
-        >>> cmdlet1._parent is None
-        True
-
-    All real attributes are prefixed with an underscore. Without underscore is
-    interpreted as the name for the next cmdlet.
-
-        >>> cmdlet1.subcmd._cmdline
-        ['cmd', 'subcmd']
-
-    After once using a cmdlet, you always get the same cmdlet
-
-        >>> cmdlet1.subcmd._marked = True
-        >>> cmdlet1.subcmd._marked
-        True
-
-    You can define aliases:
-
-        >>> cmdlet1.alias = 'much longer subcommand'
-        >>> cmdlet1.alias._cmdline
-        ['cmd', 'much longer subcommand']
-
-    XXX: not good, needs to be list elements, not space separated
-
-    It is convenient to define a root element without cmdstr:
-
-        >>> sh = Cmdlet('sh', cmdstr='')
-        >>> sh.ps.ax._cmdline
-        ['ps', 'ax']
-
-    You can execute cmds:
-
-        >>> sh.ps.ax()[0].split()[0]
-        'PID'
-
-        >>> sh.echo('foo\nbar')
-        ['foo', 'bar']
+        As with ``ps.ax`` the ``ax`` is interpreted as the name for a cmdlet
+        (see below) the normal attributes are named _<attr>. We could provide
+        an adapter to access them as normal attributes, e.g.
+        IRawCmdlet(cmdlet).<attr> = <value>. But its kind of unnecessary. Maybe
+        not...
     """
+
+    __cmdslice__ = []
+    __name__ = None
+    __parent__ = None
+
     _name = property(lambda x: x.__name__)
     _parent = property(lambda x: x.__parent__)
-    
+ 
 
-    def __init__(self, name, parent=None, cmdstr=None):
-        self.__name__ = name
-        self.__parent__ = parent
+    def __init__(self, name=None, parent=None, cmdslice=None):
+        """
+        An empty cmdlet is valid and generates an empty cmdline:
+        XXX: is that good for Popen?
+
+            >>> cmd = Cmdlet()
+            >>> cmd._name == cmd._parent == None
+            True
+            >>> cmd._cmdline
+            []
+
+        The defaults are set in a way, that inheriting classed can override
+        without defining a __init__ method:
+
+            >>> cmdslice = ['some', 'cmd']
+            >>> class SomeCmd(Cmdlet):
+            ...     __cmdslice__ = cmdslice
+            >>> s = SomeCmd()
+            >>> s.__cmdslice__ is cmdslice
+            True
+
+        Set values during __init__:
+
+            >>> name = 'name'
+            >>> parent = 'parent'
+            >>> cmd = Cmdlet(name=name,parent=parent)
+            >>> cmd._name == cmd.__name__ == 'name'
+            True
+            >>> cmd._parent == cmd.__parent__ == 'parent'
+            True
+
+        A cmdslice may be set independent of name
+
+            >>> cmdslice = 'cmdslice'
+            >>> cmd = Cmdlet(name=name,parent=parent,cmdslice=cmdslice)
+            >>> cmd._cmdslice
+            ['cmdslice']
+
+        We have a childs dictionary
+
+            >>> cmd._childs
+            {}
+        """
+        if name is not None:
+            self.__name__ = name
+            if cmdslice is None:
+                self._cmdslice = name
+        if cmdslice is not None:
+            self._cmdslice = cmdslice
+        if parent is not None:
+            self.__parent__ = parent
         self._childs = {}
-        if cmdstr is None:
-            self._cmdstr = name
-        else:
-            self._cmdstr = cmdstr
+
+
+    def _set_cmdslice(self, value):
+        """
+        A list is directly used, it is the same list you gave, i.e. you can
+        change afterwards:
+
+            >>> cmd = Cmdlet()
+            >>> slice = ['list']
+            >>> cmd._cmdslice = slice
+            >>> cmd._cmdslice is slice
+            True
+
+        A string (str/unicode) is translated into a one-element list:
+
+            >>> cmd._cmdslice = 'str'
+            >>> cmd._cmdslice
+            ['str']
+
+            >>> cmd._cmdslice = u'unicode'
+            >>> cmd._cmdslice
+            [u'unicode']
+
+        Everything else is translated into a list with list(value):
+
+            >>> cmd._cmdslice = ('tuple',)
+            >>> cmd._cmdslice
+            ['tuple']
+
+        Setting cmdslice to None clears it:
+
+            >>> cmd._cmdslice = None
+            >>> cmd._cmdslice
+            []
+        """
+        if value is None:
+            value = []
+        elif type(value) in (str, unicode):
+            value = [value]
+        elif not type(value) is list:
+            value = list(value)
+        self.__cmdslice__ = value
+
+    _cmdslice = property(
+            lambda x: x.__cmdslice__,
+            _set_cmdslice,
+            )
 
 
     @property
     def _cmdline(self):
-        # cmdline up to this node 
+        """Return the cmdline that would be executed by cmd()
+
+        The cmdline is given as a list of cmline elements:
+
+            >>> cmd = Cmdlet('cmd')
+            >>> cmd._cmdline
+            ['cmd']
+
+        A cmd uses its parent to form the cmdline.
+
+            >>> subcmd = Cmdlet('subcmd',parent=cmd)
+            >>> subcmd._cmdline
+            ['cmd', 'subcmd']
+
+            >>> subsubcmd = Cmdlet('subsubcmd',parent=subcmd)
+            >>> subsubcmd._cmdline
+            ['cmd', 'subcmd', 'subsubcmd']
+
+        Empty elements are filtered out:
+
+            >>> subcmd._cmdslice = ''
+            >>> subsubcmd._cmdline
+            ['cmd', 'subsubcmd']
+        """
         try:
-            cmdline = self.__parent__._cmdline + [self._cmdstr]
+            cmdline = self.__parent__._cmdline + self._cmdslice
         except AttributeError:
-            cmdline = [self._cmdstr]
+            cmdline = self._cmdslice
         return filter(None, cmdline)
 
 
     def __call__(self, *args, **kws):
-        fmt = kws.pop('fmt', 'stdout')
+        """ WIP, needs format and workdir
+        """
         res = _exec(*(self._cmdline+list(args)))
         self._res = res
         return [x.rstrip() for x in res['stdout'].readlines()]
@@ -159,9 +237,36 @@ class Cmdlet(object):
 
     def __getattr__(self, name):
         """return cmdlet for name
+
+        The cmdlet for subcmd is automatically created and returned
+
+            >>> cmd = Cmdlet('cmd')
+            >>> cmd.subcmd._cmdline
+            ['cmd', 'subcmd']
+
+        Once a cmdlet has been created, you'll always get the same one until
+        you delete it:
+
+            >>> cmd.subcmd == cmd.subcmd
+            True
+
+            >>> subcmd = cmd.subcmd
+            >>> del cmd.subcmd
+            >>> subcmd == cmd.subcmd
+            False
+
+        Attributes beginning with ``_`` still cause AttributeError, if
+        non-existent:
+
+            >>> getattr(cmd, '_a', 'not there')
+            'not there'
+
+        Setting an attribute beginning with ``_`` works:
+
+            >>> cmd._a = 1
+            >>> getattr(cmd, '_a', 'not there')
+            1
         """
-        # cmdlet names may not begin with an underscore
-        # use the _cmdstr attribute to achieve output with leading underscore
         if name[0] == '_':
             raise AttributeError(name)
 
@@ -172,12 +277,23 @@ class Cmdlet(object):
         return cmdlet
 
 
+    def __delattr__(self, name):
+        """Tested in __getattr__ doctest
+        """
+        del self._childs[name]
+
+
     def __setattr__(self, name, value):
-        # cmdlet names may not begin with an underscore
+        """
+            >>> cmd = Cmdlet()
+            >>> cmd.subcmd = 'subcmd'
+            >>> cmd.subcmd._cmdline
+            ['subcmd']
+        """
+        # Tested in __getattr__ doctest
         if name[0] == '_':
             object.__setattr__(self, name, value)
             return
 
-        # generate and get the child cmdlet and assign cmdstr
         child = getattr(self, name)
-        child._cmdstr = value
+        child._cmdslice = value
